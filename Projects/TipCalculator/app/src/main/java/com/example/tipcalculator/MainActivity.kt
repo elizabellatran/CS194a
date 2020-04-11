b9c339201d97d38dc560bda9f927928309ecc12e
Elizabeth <elizabethbellatran@gmail.com>
2020-04-11 09:02:08 -0700

    tip learning how to use a ConstraintLayout

---
+++ Projects/TipCalculator/app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.tipcalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// all logic is within this script
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
